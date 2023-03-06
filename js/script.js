ebff7316adeb887080041b193fbedbc0fabb303a
Mariana Tamay <maritamay4@gmail.com>
2023-03-06 17:02:43 -0300

    Responsividade do swiper

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 2.2,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 1.6,
+      spaceBetween: 10,
     },
-  });
+    420: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    500: {
+      slidesPerView: 2.3,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 2.2,
+      spaceBetween: 10,
+    },
+  },
+});
